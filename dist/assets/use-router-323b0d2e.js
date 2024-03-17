a82b5bf749d5a4fe3c7f161fd0afb30992a8a413
Odion Julius <odionjulius7@gmail.com>
2024-03-17 06:11:57 +0100

    omo!

--- dist/assets/use-router-323b0d2e.js
+++
@@ -1,1 +0,0 @@
-import{bt as o,r as t}from"./index-d14368b9.js";function s(){const r=o();return t.useMemo(()=>({back:()=>r(-1),forward:()=>r(1),reload:()=>window.location.reload(),push:e=>r(e),replace:e=>r(e,{replace:!0})}),[r])}export{s as u};
